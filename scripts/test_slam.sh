d638a80fc2c29cf295494d9839234f9dc8404a04
Georgia <georgia.gn@gmail.com>
2020-07-07 17:41:17 +0000

    added sourcing of devel/setup.bash to test_slam script

--- scripts/test_slam.sh
+++ scripts/test_slam.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-xterm  -e  " roslaunch turtlebot_gazebo turtlebot_world.launch " &
+xterm  -e  "source /opt/ros/kinetic/setup.bash; source devel/setup.bash; roslaunch turtlebot_gazebo turtlebot_world.launch " &
 sleep 5
-xterm  -e  " source /opt/ros/kinetic/setup.bash; source devel/setup.bash; roslaunch turtlebot_gazebo gmapping_demo.launch " & 
+xterm  -e  "source /opt/ros/kinetic/setup.bash; source devel/setup.bash; roslaunch turtlebot_gazebo gmapping_demo.launch " & 
 sleep 5
 xterm -e "source /opt/ros/kinetic/setup.bash; source devel/setup.bash; roslaunch turtlebot_rviz_launchers view_navigation.launch " &
 sleep 5
