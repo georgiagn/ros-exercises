a52af2782fab0da2533faf4264ff8159b2125553
Georgia <georgia.gn@gmail.com>
2020-07-21 19:01:16 +0000

    Removed sourcing of devel/setup.bash from scripts

--- scripts/pick_objects.sh
+++ scripts/pick_objects.sh
@@ -3,11 +3,11 @@
 export TURTLEBOT_GAZEBO_WORLD_FILE=/home/workspace/ros-exercises/src/my_robot/worlds/my.world
 export TURTLEBOT_GAZEBO_MAP_FILE=/home/workspace/ros-exercises/src/my_robot/maps/map.yaml
 
-xterm  -e  "source /opt/ros/kinetic/setup.bash; source devel/setup.bash; roslaunch turtlebot_gazebo  turtlebot_world.launch " &
+xterm -e "roslaunch turtlebot_gazebo  turtlebot_world.launch " &
 sleep 10
-xterm  -e  "source /opt/ros/kinetic/setup.bash; source devel/setup.bash; roslaunch turtlebot_gazebo amcl_demo.launch " & 
+xterm -e "roslaunch turtlebot_gazebo amcl_demo.launch " & 
 sleep 10
-xterm -e "source /opt/ros/kinetic/setup.bash; source devel/setup.bash; roslaunch turtlebot_rviz_launchers view_navigation.launch " &
+xterm -e "roslaunch turtlebot_rviz_launchers view_navigation.launch " &
 sleep 10
-xterm -e "source /opt/ros/kinetic/setup.bash; source devel/setup.bash; rosrun pick_objects pick_objects " &
+xterm -e "rosrun pick_objects pick_objects " &
 
--- scripts/test_slam.sh
+++ scripts/test_slam.sh
@@ -2,11 +2,11 @@
 
 export TURTLEBOT_GAZEBO_WORLD_FILE=/home/workspace/ros-exercises/src/my_robot/worlds/my.world
 
-xterm  -e  "source /opt/ros/kinetic/setup.bash; source devel/setup.bash; roslaunch turtlebot_gazebo turtlebot_world.launch " &
+xterm -e "roslaunch turtlebot_gazebo turtlebot_world.launch " &
 sleep 5
-xterm  -e  "source /opt/ros/kinetic/setup.bash; source devel/setup.bash; roslaunch turtlebot_gazebo gmapping_demo.launch " & 
+xterm -e "roslaunch turtlebot_gazebo gmapping_demo.launch " & 
 sleep 5
-xterm -e "source /opt/ros/kinetic/setup.bash; source devel/setup.bash; roslaunch turtlebot_rviz_launchers view_navigation.launch " &
+xterm -e "roslaunch turtlebot_rviz_launchers view_navigation.launch " &
 sleep 5
-xterm -e " source /opt/ros/kinetic/setup.bash; source devel/setup.bash; roslaunch turtlebot_teleop keyboard_teleop.launch " 
+xterm -e "roslaunch turtlebot_teleop keyboard_teleop.launch " 
 
